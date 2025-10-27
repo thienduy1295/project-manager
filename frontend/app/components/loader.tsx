3e77aa2837cc43eefe2fcdff3c964e38e7811681
Duy Cao <thienduy1295@gmail.com>
2025-10-27 15:17:50 +0700

    fix: improve user experience

--- frontend/app/components/loader.tsx
+++ frontend/app/components/loader.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from 'lucide-react';
 
 export const Loader = () => {
   return (
-    <div className="flex items-center justify-center h-full">
+    <div className="flex items-center justify-center min-h-screen">
       <Loader2 className="w-10 h-10 animate-spin" />
     </div>
   );
